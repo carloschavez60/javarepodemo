791fc72d6e04a12d6835265a67fd73686110ffde
Carlos Chavez Loo <carloschavez6020@gmail.com>
2025-09-01 18:01:00 -0500

    se crearon los proyectos

---
+++ proyectos/SoftProg/src/main/java/pe/edu/pucp/softprog/main/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softprog.main;
+
+import pe.edu.pucp.softprog.rrhh.model.Empleado;
+
+/**
+ *
+ * @author Carlos Chavez Loo
+ */
+public class Principal {
+    public static void main(String[] args) {
+        var empleado = new Empleado();
+    }
+}
